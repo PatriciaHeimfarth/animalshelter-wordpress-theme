ff34d242fdab428c412d4677a3767f4f8b911829
Patti85 <pbeier24@googlemail.com>
2020-12-31 16:18:43 +0100

    widgets bug

--- sidebar.php
+++ sidebar.php
@@ -6,6 +6,6 @@
 
 <?php if (is_active_sidebar( 'main-sidebar' )) : ?>
     <aside class="sidebar widget-area">
-    <?php dynamic_sidebar( 'main-sidebar') )?>
+    <?php dynamic_sidebar( 'main-sidebar')?>
     </aside>
 <?php endif; ?>
