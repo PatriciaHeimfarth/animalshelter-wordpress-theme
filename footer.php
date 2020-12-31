71a591736c6e61f7fb1c605a03280f3eb1cf74b5
Patti85 <pbeier24@googlemail.com>
2020-12-31 14:31:44 +0100

    added footer and header

--- footer.php
+++ footer.php
@@ -1,8 +1,19 @@
-<div class="footer">
-    <a href="#" class="right">Imprint</a>
-</div>
+<footer class="footer">
+    <nav class="main-navigation-container">
+        <a href="index.html"></a>
 
-<?php wp_footer()?>
+        <?php
+        wp_nav_menu(($arg = array(
+            'menu_class' => 'footer-navigation',
+            'theme_location' => 'footer'
+        )))
+        ?>
+
+    </nav>
+</footer>
+
+<?php wp_footer() ?>
 
 </body>
+
 </html>
--- header.php
+++ header.php
@@ -8,4 +8,20 @@
     <?php wp_head(); ?>
 </head>
 
+<body> 
+    <header class="header">
+        <nav class="main-navigation-container">
+            <a href="index.html"></a>
+
+            <?php
+            wp_nav_menu(  ( $arg = array(
+                'menu_class' => 'main-navigation',
+                'theme_location' => 'primary'
+            )) )
+            ?>
+
+        </nav>
+
+    </header>
+
  
