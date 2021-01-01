a527d300a394fab5be81bfe911e73414bfa65d7e
Patti85 <pbeier24@googlemail.com>
2021-01-01 19:19:25 +0100

    cat page

--- page-cats.php
+++ page-cats.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 <div class="main-wrap" role="main">
- <h1>Cats Heading</h1>
+ <h1>Cats</h1>
  <section id="animal-listing">
      <?php  $args = array(  
         'post_type' => 'animal',
@@ -15,11 +15,13 @@ get_header();
     $loop = new WP_Query( $args ); 
         
     while ( $loop->have_posts() ) : $loop->the_post(); 
-        print the_title(); 
-        the_excerpt(); 
+        if(has_post_thumbnail( )){         
+            the_post_thumbnail('full' );
+        }
+       
     endwhile;
 
-    wp_reset_postdata(); ?>
+    ?>
  </section>
 
 </div>
