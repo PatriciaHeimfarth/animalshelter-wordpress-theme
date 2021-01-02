c0d419dbe3cdfd77b0ac089ca5888e1718169a0e
Patti85 <pbeier24@googlemail.com>
2021-01-02 17:32:59 +0100

    animal detail page design

--- single-animal.php
+++ single-animal.php
@@ -1,46 +1,25 @@
 <?php get_header(); ?>
 
-<?php while ( have_posts() ) : the_post(); ?>
-<article <?php post_class() ?> id="animal-<?php the_ID(); ?>">
-    <div class="row column" id="animal-header">
-        <h1 class="entry-title"><?php the_title(); ?></h1>
-    </div>
-
-    <div class="main-wrap" role="main">
-        <div class="entry-content">
-            <div class="row">
-                <div class="medium-6 columns">
-                    <?php if(has_post_thumbnail( )) : ?>
-                        <a href="<?php the_post_thumbnail_url('large'); ?>">
-                                <img src="<?php the_post_thumbnail_url('large'); ?>"/>
-                        </a>
-                    <?php endif; ?>
+<?php while (have_posts()) : the_post(); ?>
+    <article <?php post_class() ?> id="animal-<?php the_ID(); ?>">
+        <div class="row column" id="animal-header">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+        </div>
+        <div class="main-wrap" role="main">
+            <div class="entry-content">
+                <div class="row">
+                    <div class="medium-6 columns">
+                        <?php if (has_post_thumbnail()) : ?>
+                            <img class="animal-detail-image" src="<?php the_post_thumbnail_url('full'); ?>" />
+                        <?php endif; ?>
+                    </div>
                 </div>
-                <div class="medium-6 columns">
-                    <?php //the_animal_species(get_the_ID()); ?>
+                <div class="row columns">
+                    <?php the_content(); ?>
                 </div>
-            </div>
-
-            <div class="row columns">
-                <?php the_content(); ?>
             </div>
         </div>
-
-        <footer>
-            <?php
-                wp_link_pages(
-                    array(
-                        'before' => '<nav id="page-nav"><p>' . __( 'Animals:', TRANSLATION_CONST ),
-                        'after'  => '</p></nav>',
-                    )
-                );
-            ?>
-        </footer>
-
-        <?php comments_template(); ?>
-
-    </div>
-</article>
-<?php endwhile;?>
+    </article>
+<?php endwhile; ?>
 
 <?php get_footer();
